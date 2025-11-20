8c3a35d6f38e1aeb4f05f129e4eb6fe94b56e432
Steven Sadler <stevenjsadler@gmail.com>
2025-11-20 22:15:36 +0000

    section 9 odometry

--- src/bumperbot_controller/src/simple_controller.cpp
+++ src/bumperbot_controller/src/simple_controller.cpp
@@ -1,4 +1,5 @@
 #include "bumperbot_controller/simple_controller.hpp"
+#include <tf2/LinearMath/Quaternion.hpp>
 #include <Eigen/Geometry>
 
 
@@ -6,6 +7,11 @@ using std::placeholders::_1;
 
 SimpleController::SimpleController(const std::string & name)
     : Node(name)
+    , left_wheel_prev_pos_(0.0)
+    , right_wheel_prev_pos_(0.0)
+    , x_(0.0)
+    , y_(0.0)
+    , theta_(0.0)
 {
     declare_parameter("wheel_radius", 0.038);
     declare_parameter("wheel_separation", 0.278);
@@ -16,11 +22,29 @@ SimpleController::SimpleController(const std::string & name)
     RCLCPP_INFO_STREAM(get_logger(), "Using wheel_radius " << wheel_radius_);
     RCLCPP_INFO_STREAM(get_logger(), "Using wheel_separation " << wheel_separation_);
 
+    prev_time_ = get_clock()->now();
+
     wheel_cmd_pub_ = create_publisher<std_msgs::msg::Float64MultiArray>("/simple_velocity_controller/commands", 10);
     vel_sub_ = create_subscription<geometry_msgs::msg::TwistStamped>("/bumperbot_controller/cmd_vel", 10,
         std::bind(&SimpleController::velCallback, this, _1));
+    joint_sub_ = create_subscription<sensor_msgs::msg::JointState>("/joint_states", 10,
+        std::bind(&SimpleController::jointCallback, this, _1));
+    odom_pub_ = create_publisher<nav_msgs::msg::Odometry>("/bumperbot_controller/odom", 10);
 
     speed_conversion_ << wheel_radius_/2, wheel_radius_/2, wheel_radius_/wheel_separation_, -wheel_radius_/wheel_separation_;
+
+    odom_msg_.header.frame_id = "odom";
+    odom_msg_.child_frame_id = "base_footprint";
+    odom_msg_.pose.pose.orientation.x = 0.0;
+    odom_msg_.pose.pose.orientation.y = 0.0;
+    odom_msg_.pose.pose.orientation.z = 0.0;
+    odom_msg_.pose.pose.orientation.w = 1.0;
+
+    transform_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(this);
+    transform_stamped_.header.frame_id = "odom";
+    transform_stamped_.child_frame_id = "base_footprint";
+
+
 
     RCLCPP_INFO_STREAM(get_logger(), "The conversion matrix is \n" << speed_conversion_);
 }
@@ -35,6 +59,63 @@ void SimpleController::velCallback(const geometry_msgs::msg::TwistStamped & msg)
     wheel_speed_msg.data.push_back(wheel_speed.coeff(0));
 
     wheel_cmd_pub_->publish(wheel_speed_msg);
+}
+
+void SimpleController::jointCallback(const sensor_msgs::msg::JointState & msg)
+{
+    double dp_left = msg.position.at(1) - left_wheel_prev_pos_;
+    double dp_right = msg.position.at(0) - right_wheel_prev_pos_;
+    rclcpp::Time msg_time = msg.header.stamp;
+    rclcpp::Duration dt = msg_time - prev_time_;
+
+    left_wheel_prev_pos_ = msg.position.at(1);
+    right_wheel_prev_pos_ = msg.position.at(0);
+    prev_time_ = msg_time;
+
+    double phi_left;
+    double phi_right;
+    if (dt.seconds() > 0)
+    {
+        phi_left = dp_left / dt.seconds();
+        phi_right = dp_right / dt.seconds();
+    }
+    else
+    {
+        phi_left = 0.0;
+        phi_right = 0.0;
+    }
+
+    double linear = wheel_radius_ / 2 * (phi_right + phi_left);
+    double angular = wheel_radius_ / wheel_separation_ * (phi_right - phi_left);
+
+    double d_s = wheel_radius_ / 2 * (dp_right + dp_left);
+    double d_theta = wheel_radius_ / wheel_separation_ * (dp_right - dp_left);
+    theta_ += d_theta;
+    x_ += d_s * cos(theta_);
+    y_ += d_s * sin(theta_);
+
+    tf2::Quaternion q;
+    q.setRPY(0, 0, theta_);
+    odom_msg_.pose.pose.orientation.x = q.x();
+    odom_msg_.pose.pose.orientation.y = q.y();
+    odom_msg_.pose.pose.orientation.z = q.z();
+    odom_msg_.pose.pose.orientation.w = q.w();
+    odom_msg_.header.stamp = get_clock()->now();
+    odom_msg_.pose.pose.position.x = x_;
+    odom_msg_.pose.pose.position.y = y_;
+    odom_msg_.twist.twist.linear.x = linear;
+    odom_msg_.twist.twist.angular.z = angular;
+
+    transform_stamped_.transform.translation.x = x_;
+    transform_stamped_.transform.translation.y = y_;
+    transform_stamped_.transform.rotation.x = q.x();
+    transform_stamped_.transform.rotation.y = q.y();
+    transform_stamped_.transform.rotation.z = q.z();
+    transform_stamped_.transform.rotation.w = q.w();
+    transform_stamped_.header.stamp = get_clock()->now();
+
+    odom_pub_->publish(odom_msg_);
+    transform_broadcaster_->sendTransform(transform_stamped_);
 }
 
 
